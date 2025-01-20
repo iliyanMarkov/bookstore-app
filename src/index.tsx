4daa986c0269db7ca71e3e5c8fc0996d9e21436e
iliyanMarkov <ilkomarkov000@gmail.com>
2025-01-20 10:54:13 +0200

    fetchBooks and fetchUser on App load

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import store from "./redux/store.ts";
+import { Provider } from "react-redux";
+import { fetchBooks } from "./redux/bookStore/bookStoreActions.ts";
+import { fetchUser } from "./redux/user/userActions.ts";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+
+const initializeApp = async () => {
+	root.render(<div>Loading...</div>);
+
+	await fetchBooks();
+	await fetchUser();
+
+	root.render(
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
+};
+
+initializeApp();
